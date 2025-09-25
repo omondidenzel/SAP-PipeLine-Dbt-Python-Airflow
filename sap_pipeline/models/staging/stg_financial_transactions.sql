5432dece434d602c043961f53ba71aa18ee73491
omondidenzel <omondiakwany@gmail.com>
2025-09-25 06:53:04 +0200

    Adding mart model

--- sap_pipeline/models/staging/stg_financial_transactions.sql
+++ sap_pipeline/models/staging/stg_financial_transactions.sql
@@ -1,4 +1,11 @@
-select *
+select transaction_id as id
+       , account_id::text as account_id
+       , transaction_date
+       , description
+       , transaction_type
+       , amount
+       , currency
+       , created_at::date as created_at
 from {{
   source('sap_source','financial_transactions')
 }}
